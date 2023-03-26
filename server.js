668c67f4e3bffbccc00645f6384516c3b5982d66
Amir <sefati100@gmail.com>
2023-03-26 16:12:01 +0330

    handle 404 error in route

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ mongoos.connect("mongodb://localhost/booklist");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(function (req, res) {
+  res.status(404).send({ url: req.originalUrl + " not found in app" });
+});
+
 var routes = require("./api/reoutes/booklistRoutes");
 routes(app);
 
